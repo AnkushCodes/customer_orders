41389509d331b792afe25dcc91fd5c18cc922844
ankushcodes <ankushkanchar07@gmail.com>
2020-09-29 09:36:25 +0530

    save changes

---
+++ lib/screens/customer_table/order_table_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OrderTable extends StatefulWidget {
+  @override
+  _OrderTableState createState() => _OrderTableState();
+}
+
+class _OrderTableState extends State<OrderTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
